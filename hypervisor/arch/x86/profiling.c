e3e297199bc1de65378e01444b7e0e36e5245316
Manisha <manisha.chinthapally@intel.com>
2018-10-16 19:17:54 -0700

    HV:Added support to get phy CPU, VM, tool information
    This patch includes support to following
    1. Collection of physical cpuid information
    2. Collection of List of VM's available and information
    3. Tool version information
    4. Sending tool control information to guest
    
    profiling_get_version:
    	populates the tool version and feature support information and
    	copies to guest
    
    profiling_get_pcpuid:
    	populates the physical cpu information and copies to guest
    
    profiling_get_control:
    	populates the collection switch information and	copies to guest
    
    profiling_vm_list_info:
    	Collects information of all the VM's available
    	Copies the information collected to guest
    
    Tracked-On: #1409
    Acked-by: Eddie Dong <eddie.dong@intel.com>
    Signed-off-by: Manisha <manisha.chinthapally@intel.com>

--- hypervisor/arch/x86/profiling.c
+++ hypervisor/arch/x86/profiling.c
@@ -14,7 +14,12 @@
 
 #define ACRN_DBG_PROFILING		5U
 
+#define MAJOR_VERSION			1
+#define MINOR_VERSION			0
+
 #define LVT_PERFCTR_BIT_MASK		0x10000U
+
+static uint64_t	sep_collection_switch;
 
 static uint32_t profiling_pmi_irq = IRQ_INVALID;
 
@@ -78,27 +83,137 @@ int32_t profiling_msr_ops_all_cpus(__unused struct vm *vm, __unused uint64_t add
 /*
  * Generate VM info list
  */
-int32_t profiling_vm_list_info(__unused struct vm *vm, __unused uint64_t addr)
+int32_t profiling_vm_list_info(struct vm *vm, uint64_t addr)
 {
-	/* to be implemented */
+	struct vm *tmp_vm;
+	struct vcpu *vcpu;
+	int i, j, vm_idx;
+	struct profiling_vm_info_list vm_info_list;
+
+	memset((void *)&vm_info_list, 0U, sizeof(vm_info_list));
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: entering", __func__);
+
+	if (copy_from_gpa(vm, &vm_info_list, addr, sizeof(vm_info_list)) != 0) {
+		pr_err("%s: Unable to copy addr from vm\n", __func__);
+		return -EINVAL;
+	}
+
+	vm_idx = 0;
+	vm_info_list.vm_list[vm_idx].vm_id = 0xFFFFFFFFU;
+	memcpy_s(vm_info_list.vm_list[vm_idx].vm_name, 4, "VMM\0", 4);
+	for (i = 0; i < (int)phys_cpu_num; i++) {
+		vm_info_list.vm_list[vm_idx].cpu_map[i].vcpu_id = i;
+		vm_info_list.vm_list[vm_idx].cpu_map[i].pcpu_id = i;
+		vm_info_list.vm_list[vm_idx].cpu_map[i].apic_id
+			= per_cpu(lapic_id, i);
+	}
+	vm_info_list.vm_list[vm_idx].num_vcpus = i;
+	vm_info_list.num_vms = 1;
+
+	for (j = 0; j < (int)CONFIG_MAX_VM_NUM; j++) {
+		i = 0;
+
+		tmp_vm = get_vm_from_vmid(j);
+
+		if (tmp_vm == NULL) {
+			break;
+		}
+		vm_info_list.num_vms++;
+		vm_idx++;
+
+		vm_info_list.vm_list[vm_idx].vm_id = tmp_vm->vm_id;
+		memcpy_s(vm_info_list.vm_list[vm_idx].guid, 16, tmp_vm->GUID, 16);
+		snprintf(vm_info_list.vm_list[vm_idx].vm_name, 16, "vm_%d",
+				tmp_vm->vm_id, 16);
+		vm_info_list.vm_list[vm_idx].num_vcpus = 0;
+		foreach_vcpu(i, tmp_vm, vcpu) {
+			vm_info_list.vm_list[vm_idx].cpu_map[i].vcpu_id
+					= vcpu->vcpu_id;
+			vm_info_list.vm_list[vm_idx].cpu_map[i].pcpu_id
+					= vcpu->pcpu_id;
+			vm_info_list.vm_list[vm_idx].cpu_map[i].apic_id = 0;
+			vm_info_list.vm_list[vm_idx].num_vcpus++;
+		}
+	}
+
+	if (copy_to_gpa(vm, &vm_info_list, addr, sizeof(vm_info_list)) != 0) {
+		pr_err("%s: Unable to copy addr to vm\n", __func__);
+		return -EINVAL;
+	}
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: exiting", __func__);
 	return 0;
 }
 
 /*
  * Sep/socwatch profiling version
  */
-int32_t profiling_get_version(__unused struct vm *vm, __unused uint64_t addr)
+int32_t profiling_get_version(struct vm *vm, uint64_t addr)
 {
-	/* to be implemented */
+	struct profiling_version_info ver_info;
+
+	memset((void *)&ver_info, 0U, sizeof(ver_info));
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: entering", __func__);
+
+	if (copy_from_gpa(vm, &ver_info, addr, sizeof(ver_info)) != 0) {
+		pr_err("%s: Unable to copy addr from vm\n", __func__);
+		return -EINVAL;
+	}
+
+	ver_info.major = MAJOR_VERSION;
+	ver_info.minor = MINOR_VERSION;
+	ver_info.supported_features = (int64_t)
+					(1 << CORE_PMU_SAMPLING) |
+					(1 << CORE_PMU_COUNTING) |
+					(1 << LBR_PMU_SAMPLING) |
+					(1 << VM_SWITCH_TRACING);
+
+	if (copy_to_gpa(vm, &ver_info, addr, sizeof(ver_info)) != 0) {
+		pr_err("%s: Unable to copy addr to vm\n", __func__);
+		return -EINVAL;
+	}
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: exiting", __func__);
+
 	return 0;
 }
 
 /*
  * Gets type of profiling - sep/socwatch
  */
-int32_t profiling_get_control(__unused struct vm *vm, __unused uint64_t addr)
+int32_t profiling_get_control(struct vm *vm, uint64_t addr)
 {
-	/* to be implemented */
+	struct profiling_control prof_control;
+
+	memset((void *)&prof_control, 0U, sizeof(prof_control));
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: entering", __func__);
+
+	if (copy_from_gpa(vm, &prof_control, addr, sizeof(prof_control)) != 0) {
+		pr_err("%s: Unable to copy addr from vm\n", __func__);
+		return -EINVAL;
+	}
+
+	switch (prof_control.collector_id) {
+	case COLLECT_PROFILE_DATA:
+		prof_control.switches = sep_collection_switch;
+		break;
+	case COLLECT_POWER_DATA:
+		break;
+	default:
+		pr_err("%s: unknown collector %d",
+			__func__, prof_control.collector_id);
+	}
+
+	if (copy_to_gpa(vm, &prof_control, addr, sizeof(prof_control)) != 0) {
+		pr_err("%s: Unable to copy addr to vm\n", __func__);
+		return -EINVAL;
+	}
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: exiting", __func__);
+
 	return 0;
 }
 
@@ -136,9 +251,29 @@ int32_t profiling_config_vmsw(__unused struct vm *vm, __unused uint64_t addr)
 /*
  * Get the physical cpu id
  */
-int32_t profiling_get_pcpuid(__unused struct vm *vm, __unused uint64_t addr)
+int32_t profiling_get_pcpuid(struct vm *vm, uint64_t addr)
 {
-	/* to be implemented */
+	struct profiling_pcpuid pcpuid;
+
+	memset((void *)&pcpuid, 0U, sizeof(pcpuid));
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: entering", __func__);
+
+	if (copy_from_gpa(vm, &pcpuid, addr, sizeof(pcpuid)) != 0) {
+		pr_err("%s: Unable to copy addr from vm\n", __func__);
+		return -EINVAL;
+	}
+
+	cpuid_subleaf(pcpuid.leaf, pcpuid.subleaf, &pcpuid.eax,
+			&pcpuid.ebx, &pcpuid.ecx, &pcpuid.edx);
+
+	if (copy_to_gpa(vm, &pcpuid, addr, sizeof(pcpuid)) != 0) {
+		pr_err("%s: Unable to copy param to vm\n", __func__);
+		return -EINVAL;
+	}
+
+	dev_dbg(ACRN_DBG_PROFILING, "%s: exiting", __func__);
+
 	return 0;
 }
 
--- hypervisor/include/arch/x86/profiling.h
+++ hypervisor/include/arch/x86/profiling.h
@@ -9,6 +9,12 @@
 
 #ifdef HV_DEBUG
 
+#define MAX_NR_VCPUS			8
+#define MAX_NR_VMS				6
+
+#define COLLECT_PROFILE_DATA	0
+#define COLLECT_POWER_DATA		1
+
 typedef enum IPI_COMMANDS {
 	IPI_MSR_OP = 0,
 	IPI_PMU_CONFIG,
@@ -18,6 +24,57 @@ typedef enum IPI_COMMANDS {
 	IPI_UNKNOWN,
 } ipi_commands;
 
+typedef enum PROFILING_SEP_FEATURE {
+	CORE_PMU_SAMPLING = 0U,
+	CORE_PMU_COUNTING,
+	PEBS_PMU_SAMPLING,
+	LBR_PMU_SAMPLING,
+	UNCORE_PMU_SAMPLING,
+	VM_SWITCH_TRACING,
+	MAX_SEP_FEATURE_ID
+} profiling_sep_feature;
+
+struct profiling_version_info {
+	int32_t major;
+	int32_t minor;
+	int64_t supported_features;
+	int64_t reserved;
+};
+
+struct profiling_pcpuid {
+	uint32_t leaf;
+	uint32_t subleaf;
+	uint32_t eax;
+	uint32_t ebx;
+	uint32_t ecx;
+	uint32_t edx;
+};
+
+struct profiling_control {
+	int32_t		collector_id;
+	int32_t		reserved;
+	uint64_t	switches;
+};
+
+struct profiling_vcpu_pcpu_map {
+	int32_t vcpu_id;
+	int32_t pcpu_id;
+	int32_t apic_id;
+};
+
+struct profiling_vm_info {
+	int32_t		vm_id;
+	unsigned char	guid[16];
+	char		vm_name[16];
+	int32_t		num_vcpus;
+	struct profiling_vcpu_pcpu_map	cpu_map[MAX_NR_VCPUS];
+};
+
+struct profiling_vm_info_list {
+	int32_t num_vms;
+	struct profiling_vm_info vm_list[MAX_NR_VMS];
+};
+
 /*
  * Wrapper containing  SEP sampling/profiling related data structures
  */
@@ -25,16 +82,13 @@ struct profiling_info_wrapper {
 	ipi_commands			ipi_cmd;
 };
 
-int32_t profiling_get_version(__unused struct vm *vm,
-		__unused uint64_t addr);
-int32_t profiling_get_pcpuid(__unused struct vm *vm,
-		__unused uint64_t addr);
+
+int32_t profiling_get_version(struct vm *vm, uint64_t addr);
+int32_t profiling_get_pcpuid(struct vm *vm, uint64_t addr);
 int32_t profiling_msr_ops_all_cpus(__unused struct vm *vm,
 		__unused uint64_t addr);
-int32_t profiling_vm_list_info(__unused struct vm *vm,
-		__unused uint64_t addr);
-int32_t profiling_get_control(__unused struct vm *vm,
-		__unused uint64_t addr);
+int32_t profiling_vm_list_info(struct vm *vm, uint64_t addr);
+int32_t profiling_get_control(struct vm *vm, uint64_t addr);
 int32_t profiling_set_control(__unused struct vm *vm,
 		__unused uint64_t addr);
 int32_t profiling_config_pmi(__unused struct vm *vm,
